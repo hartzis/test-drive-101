35a11462bf66059b021e2e6a1f6fe8bd9e2862ab
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-14 15:38:32 -0600

    made splice one line

--- part2/src.js
+++ part2/src.js
@@ -2,26 +2,26 @@ var splice = function(arr, start, numToReplace, replace) {
 
 
 
-    var newArray = [];
+    // var newArray = [];
 
-    var replaceArray = [];
-    for (var i = 3; i < arguments.length; i++) {
-        replaceArray = replaceArray.concat(arguments[i])
-    };
+    // var replaceArray = [];
+    // for (var i = 3; i < arguments.length; i++) {
+    //     replaceArray = replaceArray.concat(arguments[i])
+    // };
 
-    newArray = (arr.slice(0, start));
-    if (replace !== undefined) {
-        console.log(arguments);
-        newArray = newArray.concat(replaceArray);
+    // newArray = (arr.slice(0, start));
+    // if (replace !== undefined) {
+    //     console.log(arguments);
+    //     newArray = newArray.concat(replaceArray);
 
-    }
+    // }
 
-    newArray = newArray.concat(arr.slice(numToReplace + start));
-
-
+    // newArray = newArray.concat(arr.slice(numToReplace + start));
 
 
-    return newArray;
 
-    // return arr.slice(0, start).concat(replace).concat(arr.slice(numToReplace + start));
+
+    // return newArray;
+
+    return arr.slice(0, start).concat(Array.prototype.slice.call(arguments, 3)).concat(arr.slice(numToReplace + start));
 };
